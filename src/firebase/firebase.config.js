3862598759385b590b5c3f9361b88bc9bb41d0a6
Shakhawat <syedarafatcse@gmail.com>
2023-05-04 12:32:27 +0600

    environment variable adde fro protrct api info

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('Environment variable', import.meta.env.VITE_PASS);
 const firebaseConfig = {
-   apiKey: "AIzaSyAiqCHyOcEF_gMM17ojzM3xyDKu0ri8BNE",
-   authDomain: "chef-recipe-hunter-clien-ef342.firebaseapp.com",
-   projectId: "chef-recipe-hunter-clien-ef342",
-   storageBucket: "chef-recipe-hunter-clien-ef342.appspot.com",
-   messagingSenderId: "478889928245",
-   appId: "1:478889928245:web:8991480223dc33e816b37c"
+   apiKey: import.meta.env.VITE_APIKEY,
+   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+   projectId: import.meta.env.VITE_PROJECTID,
+   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+   appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
